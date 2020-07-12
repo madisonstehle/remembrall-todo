d0987bcba57ce27f0e1c93639e162c2f5e89e28c
Madison Stehle <mnstehle3@gmail.com>
2020-07-12 11:40:05 -0700

    thing renders on page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-
 import './styles/styles.scss';
 
 // import Header from './components/Header.js';
+import ToDo from './components/ToDo.js';
 // import Footer from './components/Footer.js';
 
-import EntryForm from './components/EntryForm.js';
 
 function App() {
   return (
     <div className="App">
       {/* <Header /> */}
-      <EntryForm />
+      <ToDo />
       {/* <Footer /> */}
     </div>
   );
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,1 +1,12 @@
 import React from 'react';
+
+function Item(props) {
+  return (
+    <div>
+      <h1>{props.task.title}</h1>
+      <p>{props.task.description}</p>
+    </div>
+  )
+}
+
+export default Item;
